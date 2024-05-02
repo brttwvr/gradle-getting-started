72a73788a82655e22b220a18f5645088c8439c20
brttwvr <brettweaver@u.boisestate.edu>
2024-05-01 20:50:27 -0600

    test

--- src/main/java/com/example/heroku/HerokuApplication.java
+++ src/main/java/com/example/heroku/HerokuApplication.java
@@ -67,7 +67,7 @@ public class HerokuApplication {
 
       ArrayList<String> output = new ArrayList<String>();
       while (rs.next()) {
-        output.add("Read from DB: " + rs.getTimestamp("tick") + rs.getString("random_string"));
+        output.add("Read from DB: " + rs.getTimestamp("tick") + rs.getString("random_string") + "test");
       }
 
       model.put("records", output);
